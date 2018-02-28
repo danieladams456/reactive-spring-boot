00df2475d8ca39095119036c070741514477d0fd
Daniel Adams <daniel.adams@genworth.com>
2018-02-28 14:57:10 -0500

    synchronous method

--- src/main/java/com/genworth/example/reactive/ReactiveRestController.java
+++ src/main/java/com/genworth/example/reactive/ReactiveRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReactiveRestController {
 	
-	@GetMapping
+	@GetMapping("/sync")
 	public ResponseDTO test() {
 		return ResponseDTO.builder()
 				.status("ok")
